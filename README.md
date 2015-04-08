5ee87bb0b9721e21f13a0f6f8e288935563abfce
Michael Klein <mbklein@gmail.com>
2015-04-08 17:48:29 -0500

    First usable version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Cahdmaker
 
-TODO: Write a gem description
+Create card images for use with a [party game for horrible people](http://cardsagainsthumanity.com).
 
 ## Installation
 
@@ -20,7 +20,28 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+### Code
+
+```ruby
+require 'cahdmaker'
+
+maker = Cahdmaker::Maker.new()
+
+# Generate in-memory card images as RMagick Magick::Image objects
+white_card = maker.white("31 flavors of hate")
+black_card = maker.black("I would have gotten away with it if it weren't for _.")
+pick2_card = maker.black2("I would have gotten away with _ if it weren't for _.")
+pick3_card = maker.black3("I would have gotten away with _ if it weren't for _ and _.")
+
+# Save image to file
+white_card.write('/path/to/white_card.png')
+
+# Grab image PNG data
+data = white_card.to_blob
+
+# Block-driven
+maker.white('31 flavors of hate') { |card| card.write('/path/to/white_card.png') }
+```
 
 ## Contributing
 
--- lib/cahdmaker.rb
+++ lib/cahdmaker.rb
@@ -1,50 +1,54 @@
 require "cahdmaker/version"
 require 'RMagick'
+require 'tempfile'
 
 module Cahdmaker
   class Maker
     attr_reader :opts
     
     def initialize(opts={})
-      @opts = { magick: '/usr/local/bin/convert', text_size: 60, blank_size: 8 }.merge(opts)
+      @opts = { text_size: 60, blank_size: 10, font: 'Helvetica-Bold' }.merge(opts)
     end
     
-    def black(text, pick=1)
-      make_card(text, "Black#{pick}")
+    def black(text, pick=1, &block)
+      make_card(text, "Black#{pick}", &block)
     end
     
-    def black2(text)
-      black(text, 2)
+    def black2(text, &block)
+      black(text, 2, &block)
     end
     
-    def black3(text)
-      black(text, 3)
+    def black3(text, &block)
+      black(text, 3, &block)
     end
     
-    def white(text)
-      make_card(text, "White")
+    def white(text, &block)
+      make_card(text, "White", &block)
     end
     
     def make_card(text, source_card)
-      text_size = opts[:text_size]
+      text_size = opts[:text_size].to_i
       color = source_card =~ /White/i ? 'black' : 'white'
+      font = opts[:font]
       text.gsub! /\b_+\b/, ('_' * opts[:blank_size])
 
-      capfile = File.join(Dir.tmpdir, Dir::Tmpname.make_tmpname(['caption','.png'],nil))
-      args = [
-        opts[:magick], '-background', 'transparent',  '-size', '600x600', 
-        '-pointsize', text_size.to_s, '-fill', color.to_s, 
-        '-interline-spacing', (text_size/3).to_s,
-        '-font', 'Helvetica-Bold',
-        "caption:#{text}", capfile
-      ]
-      Kernel.system(*args)
-      cap = Magick::Image.read(capfile).first
+      cap = Magick::Image.read(%{caption:#{text}}) do
+        self.size = '600x'
+        self.background_color = 'transparent'
+        self.pointsize = text_size.to_f
+        self['interline-spacing'] = (text_size/3).to_f
+        self.font = font
+        self.fill = color
+      end.first
 
       img = Magick::Image.read(File.expand_path("../../cards/CAH_#{source_card}.png",__FILE__)).first
       img.x_resolution = img.y_resolution = 300
       img.composite!(cap, 75, 75, Magick::OverCompositeOp)
-      img.to_blob
+      if block_given?
+        yield(img)
+      else
+        img
+      end
     end
   end
 end
